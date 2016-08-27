52a69c932f97d7fb998183b1c569d4543e23b0a4
hansensen <1181040659@qq.com>
2016-08-27 17:13:12 +0800

    创建了Dog 类

---
+++ meituan/meituan/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  meituan
+//
+//  Created by 韩森 on 16/8/27.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ meituan/meituan/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  meituan
+//
+//  Created by 韩森 on 16/8/27.
+//  Copyright © 2016年 hansen. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
